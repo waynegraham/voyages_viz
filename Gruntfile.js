5a4e25c41ce40abd2f48e5985063345f692af546
Wayne Graham <wayne.graham@virginia.edu>
2015-04-28 11:32:29 -0400

    update packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bowercopy');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
